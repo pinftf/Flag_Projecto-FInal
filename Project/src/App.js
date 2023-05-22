063b6c442219db8bd6f9b038bb8741ad51dd4760
João Pinto <pinftf00@gmail.com>
2023-05-22 06:11:30 +0100

    navbar

--- Project/src/App.js
+++ Project/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./Components/Homepage/homePage";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Navbar />
     </div>
   );
 }
