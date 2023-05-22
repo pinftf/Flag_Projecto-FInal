28be1a3973e05f0c309bfd4b74d9dcbf380b74da
João Pinto <pinftf00@gmail.com>
2023-05-22 19:50:56 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1></h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMenu = () => setClick(false);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container"></div>
+        <Link to="/" className="navbar-logo">
+          <img src={require("../images/starwarslogo2.avif")} alt="logo" />
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <FontAwesomeIcon icon={click ? faTimes : faBars} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMenu}>
+              Home
+            </Link>
+            <li className="nav-item" />
+            <Link to="quiz" className="nav-links" onClick={closeMenu}>
+              Character Quiz
+            </Link>
+            <li className="nav-item" />
+            <Link to="lightsaber" className="nav-links" onClick={closeMenu}>
+              Lightsaber Duel
+            </Link>
+            <li className="nav-item" />
+            <Link to="soundboard" className="nav-links" onClick={closeMenu}>
+              SoundBoard
+            </Link>
+            <li className="nav-item" />
+            <Link to="translator" className="nav-links" onClick={closeMenu}>
+              Galactic Translator
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
